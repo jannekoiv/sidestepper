3d1e02ce74bcb3650d4df64abc3ca95dea4e664e
Janne Koivuranta <jannekoiv@gmail.com>
2016-02-09 06:54:49 +0200

    Added collision test to bounding box.

--- public_html/boundingBox.js
+++ public_html/boundingBox.js
@@ -12,5 +12,16 @@ var boundingBox = {
         newObject.position = vector2.create(position.x, position.y);
         newObject.size = vector2.create(size.x, size.y);
         return newObject;
+    },
+    testCollision: function (boundingBox) {
+        var halfSizeSum = vector2.create(this.size.x + boundingBox.size.x, this.size.y + boundingBox.size.y);
+        halfSizeSum.divide(2.0);
+        var distance = vector2.create(this.position.x, this.position.y);
+        distance.subtract(boundingBox.position);
+        if (Math.abs(distance.x) < halfSizeSum.x) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
--- test/boundingBoxSpec.js
+++ test/boundingBoxSpec.js
@@ -8,10 +8,34 @@
 
 describe("create", function () {
     it("constructs bounding box from center position and size.", function () {
-        var box = boundingBox.create(vector2.create(ONE, TWO), vector2.create(THREE, FOUR)); 
+        var box = boundingBox.create(vector2.create(ONE, ONE_AND_HALF), vector2.create(TWO, THREE)); 
         expect(box.position.x).toEqual(ONE);
-        expect(box.position.y).toEqual(TWO);
-        expect(box.size.x).toEqual(THREE);
-        expect(box.size.y).toEqual(FOUR);
+        expect(box.position.y).toEqual(ONE_AND_HALF);
+        expect(box.size.x).toEqual(TWO);
+        expect(box.size.y).toEqual(THREE);
+    });
+});
+
+describe("testCollision", function () {
+    it("tests collision between two bounding boxes and returns result.", function () {
+        var boundingBoxA = boundingBox.create(vector2.create(ONE, ONE), vector2.create(ONE, ONE)); 
+        var boundingBoxB = boundingBox.create(vector2.create(THREE, ONE), vector2.create(ONE, ONE)); 
+        expect(boundingBoxA.testCollision(boundingBoxB)).toEqual(false);
+    });
+});
+
+describe("testCollision", function () {
+    it("tests collision between two bounding boxes and returns result.", function () {
+        var boundingBoxA = boundingBox.create(vector2.create(ONE, ONE), vector2.create(ONE, ONE)); 
+        var boundingBoxB = boundingBox.create(vector2.create(TWO, ONE), vector2.create(ONE, ONE)); 
+        expect(boundingBoxA.testCollision(boundingBoxB)).toEqual(false);
+    });
+});
+
+describe("testCollision", function () {
+    it("tests collision between two bounding boxes and returns result.", function () {
+        var boundingBoxA = boundingBox.create(vector2.create(ONE, ONE), vector2.create(ONE, ONE)); 
+        var boundingBoxB = boundingBox.create(vector2.create(ONE_AND_HALF, ONE), vector2.create(ONE, ONE)); 
+        expect(boundingBoxA.testCollision(boundingBoxB)).toEqual(true);
     });
 });
