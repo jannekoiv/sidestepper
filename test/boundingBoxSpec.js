f4bc7e25e3fac5ba07d5342a78f3ff3355edb1ef
Janne Koivuranta <jannekoiv@gmail.com>
2016-02-10 01:57:44 +0200

    Made bounding box tests cleaner, removed overly verbosive constants.

--- test/boundingBoxSpec.js
+++ test/boundingBoxSpec.js
@@ -4,38 +4,38 @@
  * and open the template in the editor.
  */
 
-/* global boundingBox, vector2, ONE, TWO, THREE, FOUR */
+/* global boundingBox, vector2, expect */
 
 describe("create", function () {
     it("constructs bounding box from center position and size.", function () {
-        var box = boundingBox.create(vector2.create(ONE, ONE_AND_HALF), vector2.create(TWO, THREE)); 
-        expect(box.position.x).toEqual(ONE);
-        expect(box.position.y).toEqual(ONE_AND_HALF);
-        expect(box.size.x).toEqual(TWO);
-        expect(box.size.y).toEqual(THREE);
+        var box = boundingBox.create(vector2.create(1.0, 2.0), vector2.create(3.0, 4.0)); 
+        expect(box.position.x).toEqual(1.0);
+        expect(box.position.y).toEqual(2.0);
+        expect(box.size.x).toEqual(3.0);
+        expect(box.size.y).toEqual(4.0);
     });
 });
 
 describe("testCollision", function () {
     it("tests collision between two bounding boxes and returns result.", function () {
-        var boundingBoxA = boundingBox.create(vector2.create(ONE, ONE), vector2.create(ONE, ONE)); 
-        var boundingBoxB = boundingBox.create(vector2.create(THREE, ONE), vector2.create(ONE, ONE)); 
+        var boundingBoxA = boundingBox.create(vector2.create(0.9, 0.9), vector2.create(1.0, 1.0)); 
+        var boundingBoxB = boundingBox.create(vector2.create(2.0, 2.0), vector2.create(1.0, 1.0)); 
         expect(boundingBoxA.testCollision(boundingBoxB)).toEqual(false);
     });
 });
 
 describe("testCollision", function () {
     it("tests collision between two bounding boxes and returns result.", function () {
-        var boundingBoxA = boundingBox.create(vector2.create(ONE, ONE), vector2.create(ONE, ONE)); 
-        var boundingBoxB = boundingBox.create(vector2.create(TWO, ONE), vector2.create(ONE, ONE)); 
+        var boundingBoxA = boundingBox.create(vector2.create(1.0, 1.0), vector2.create(1.0, 1.0)); 
+        var boundingBoxB = boundingBox.create(vector2.create(2.0, 2.0), vector2.create(1.0, 1.0)); 
         expect(boundingBoxA.testCollision(boundingBoxB)).toEqual(false);
     });
 });
 
 describe("testCollision", function () {
     it("tests collision between two bounding boxes and returns result.", function () {
-        var boundingBoxA = boundingBox.create(vector2.create(ONE, ONE), vector2.create(ONE, ONE)); 
-        var boundingBoxB = boundingBox.create(vector2.create(ONE_AND_HALF, ONE), vector2.create(ONE, ONE)); 
+        var boundingBoxA = boundingBox.create(vector2.create(1.1, 1.1), vector2.create(1.0, 1.0)); 
+        var boundingBoxB = boundingBox.create(vector2.create(2.0, 2.0), vector2.create(1.0, 1.0)); 
         expect(boundingBoxA.testCollision(boundingBoxB)).toEqual(true);
     });
 });
