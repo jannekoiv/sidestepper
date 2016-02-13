aba0d4076c86c0d150aa598bcb88781e008ee72b
Janne Koivuranta <jannekoiv@gmail.com>
2016-02-14 00:22:27 +0200

    Renamed factory method.

--- test/vectorSpec.js
+++ test/vectorSpec.js
@@ -1,36 +1,30 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/* global vector2, expect */
 
 var SQUARE_ROOT_OF_TWO = Math.sqrt(2.0);
 
-describe("Vector", function() {
-    it("Constructs vector from x and y.", function() {
-        var vector = Vector(1.0, 2.0);
+describe("createcreateVector", function() {
+    it("creates new vector from x and y", function() {
+        var vector = createVector(1.0, 2.0);
         expect(vector.x).toBeCloseTo(1.0);
         expect(vector.y).toBeCloseTo(2.0);
     });
 });
 
 describe("add", function() {
-    it("Adds second vector to first vector and returns first vector.", function() {
-        var vectorA = Vector(1.0, 2.0);
-        var vectorB = Vector(0.1, 0.2);
+    it("adds second vector to first vector and returns first vector", function() {
+        var vectorA = createVector(1.0, 2.0);
+        var vectorB = createVector(0.1, 0.2);
         var result = vectorA.add(vectorB);
         expect(result.x).toBeCloseTo(1.1);
         expect(result.y).toBeCloseTo(2.2);
-        var hoo = Vector(1,1);
+        var hoo = createVector(1,1);
     });
 });
 
 describe("subtract", function() {
-    it("Subtracts second vector from first vector and returns first vector.", function() {
-        var vectorA = Vector(1.0, 2.0);
-        var vectorB = Vector(0.1, 0.2);
+    it("subtracts second vector from first vector and returns first vector", function() {
+        var vectorA = createVector(1.0, 2.0);
+        var vectorB = createVector(0.1, 0.2);
         var result = vectorA.subtract(vectorB);
         expect(result.x).toBeCloseTo(0.9);
         expect(result.y).toBeCloseTo(1.8);
@@ -38,8 +32,8 @@ describe("subtract", function() {
 });
 
 describe("multiply", function() {
-    it("Multiplies vector by scalar and returns it.", function() {
-        var vector = Vector(1.0, 2.0);
+    it("multiplies vector by scalar and returns it", function() {
+        var vector = createVector(1.0, 2.0);
         var scalar = 10.0;
         var result = vector.multiply(scalar);
         expect(result.x).toBeCloseTo(10);
@@ -48,8 +42,8 @@ describe("multiply", function() {
 });
 
 describe("divide", function() {
-    it("Divides vector by scalar and returns it.", function() {
-        var vector = Vector(10.0, 20.0);
+    it("divides vector by scalar and returns it", function() {
+        var vector = createVector(10.0, 20.0);
         var scalar = 10.0;
         var result = vector.divide(scalar);
         expect(result.x).toBeCloseTo(1.0);
@@ -58,16 +52,16 @@ describe("divide", function() {
 });
 
 describe("length", function() {
-    it("Returns length of vector.", function() {
-        var vector = Vector(1.0, 1.0);
+    it("returns length of vector", function() {
+        var vector = createVector(1.0, 1.0);
         var result = vector.length();
         expect(result).toBeCloseTo(SQUARE_ROOT_OF_TWO);
     });
 });
 
 describe("normalize", function() {
-    it("Normalizes vector and returns it.", function() {
-        var vector = Vector(1.0, 1.0);
+    it("normalizes vector and returns it", function() {
+        var vector = createVector(1.0, 1.0);
         var result = vector.normalize();
         expect(result.x).toBeCloseTo(SQUARE_ROOT_OF_TWO);
         expect(result.y).toBeCloseTo(SQUARE_ROOT_OF_TWO);
