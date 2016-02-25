013213f895f862f8d6afe278f4b15321f3af2d3c
Janne Koivuranta <jannekoiv@gmail.com>
2016-02-25 12:38:11 +0200

    Separated static and dynamic tiles, added support for "Tiled" json files.

---
+++ public_html/dynamicTile.js
@@ -0,0 +1,27 @@
+/*
+ */
+
+var TILE_SIZE_X = 32;
+var TILE_SIZE_Y = 32;
+
+var DynamicTile = {
+    create: function(position, velocity, tileNumber) {
+        var newObject = Object.create(DynamicTile);
+        newObject.image = AnimatedImage.create('tile1.bmp');
+        newObject.position = position.clone();
+        newObject.velocity = velocity.clone();
+        newObject.tileNumber = tileNumber;
+        newObject.size = Vector.create(TILE_SIZE_X, TILE_SIZE_Y);
+        return newObject;
+    },
+    update: function () {
+        this.position = this.position.add(this.velocity)
+    },
+    draw: function () {
+        this.image.draw(this.position, this.tileNumber);
+    },
+    calculateCenterPosition: function () {
+        return this.position.add(this.size.multiply(0.5));
+    }
+};
+
